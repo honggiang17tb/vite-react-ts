6ad45c16bb1b62d6fcd6054f06bf24babbffe81b
honggiang17tb <honggiang17tb@gmail.com>
2023-08-21 00:17:11 +0700

    eslint,prettier,lint-staged,husky,commitlint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,18 +1,65 @@
 module.exports = {
-  root: true,
-  env: { browser: true, es2020: true },
-  extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended',
-  ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
-  rules: {
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
+    root: true,
+    env: { browser: true, es2020: true },
+    extends: [
+        'eslint:recommended',
+        'plugin:react/recommended',
+        'plugin:react-hooks/recommended',
+        'plugin:@typescript-eslint/recommended',
+        'plugin:prettier/recommended',
+        'plugin:security/recommended',
+        'plugin:import/recommended',
     ],
-  },
-}
+    ignorePatterns: ['dist', '.eslintrc.cjs'],
+    parser: '@typescript-eslint/parser',
+    plugins: ['react-refresh', 'prettier', 'import', 'simple-import-sort', 'react-hooks', '@typescript-eslint'],
+    rules: {
+        'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+        'simple-import-sort/imports': 'error',
+        'simple-import-sort/exports': 'error',
+        'prettier/prettier': [
+            'warn',
+            {
+                semi: true,
+                tabWidth: 4,
+                printWidth: 120,
+                endOfLine: 'auto',
+                bracketSpacing: true,
+                arrowParens: 'always',
+                singleQuote: true,
+            },
+        ],
+    },
+    settings: {
+        react: { version: 'detect' },
+    },
+    overrides: [
+        {
+            files: ['*.js', '*.jsx', '*.ts', '*.tsx'],
+            rules: {
+                'simple-import-sort/imports': [
+                    'error',
+                    {
+                        groups: [
+                            [
+                                // Packages `react` related packages come first.
+                                '^react',
+                                '^@?\\w',
+                                // Internal packages.
+                                '^(@|components)(/.*|$)',
+                                // Side effect imports.
+                                '^\\u0000',
+                                // Style imports.
+                                '^.+\\.?(css)$',
+                            ],
+                            // Parent imports. Put `..` last.
+                            ['^\\.\\.(?!/?$)', '^\\.\\./?$'],
+                            // Other relative imports. Put same-folder imports and `.` last.
+                            ['^\\./(?=.*/)(?!/?$)', '^\\.(?!/?$)', '^\\./?$'],
+                        ],
+                    },
+                ],
+            },
+        },
+    ],
+};
